b97822d24c05d72cad38f25a740660e9ca4e3afd
Pedro Picorelli <pedro.picorelli@hotmail.com>
2024-08-13 22:26:23 -0300

    Componente Lista

---
+++ src/components/Lista/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+function Lista() {
+    return(
+        <aside>
+            <h2> Estudos do dia </h2>
+            <ul>
+                <li>
+                    <h3>
+                        React
+                    </h3>
+                    <span>
+                        02:00:22
+                    </span>
+                </li>
+                <li>
+                    <h3>
+                        JS
+                    </h3>
+                    <span>
+                        01:00:00
+                    </span>
+                </li>
+                <li>
+                    <h3>
+
+                    </h3>
+                    <span>
+
+                    </span>
+                </li>
+            </ul>
+        </aside>
+    )
+}
+
+export default Lista;
